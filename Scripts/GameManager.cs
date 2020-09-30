3bd184d1b93910060bab7cad72c011f775448256
SumeetDevStuff <71875193+SumeetDevStuff@users.noreply.github.com>
2020-09-30 14:13:06 +0530

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float slowmotion = 10f;
+
+    public void GameOver ()
+    {
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel ()
+    {
+        Time.timeScale = 1f / slowmotion;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowmotion;
+
+        yield return new WaitForSeconds(1f / slowmotion);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowmotion;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
